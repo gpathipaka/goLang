fa3d6e193370e22403553e89a0858b50c38e2c99
gpathipaka <gangadharind@gmail.com>
2019-01-29 12:18:32 -0500

    more files

---
+++ go-projects/ex1.go
@@ -0,0 +1,20 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var a = "Hello"
+	fmt.Println(a)
+
+	var x, y, z int = 1, 3, 4
+	fmt.Println(z, y, x)
+
+	var d = false
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
---
+++ go-projects/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hellow " + "World!!")
+
+	fmt.Println("2 + 3 = ", 2 + 3)
+
+	fmt.Println(true && true)
+
+	fmt.Println(!true)
+}
